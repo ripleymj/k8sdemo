42360c732d95ceb246de6dd77728b9f2f99c33d4
Stephen Mitchell <stephenmitchell2299@gmail.com>
2020-02-12 19:33:51 -0500

    goodbye world

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+
 app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
+    r = requests.get('https://finance.yahoo.com/quote/BTC-USD?p=BTC-USD')
+    data = r.text
+    soup = BeautifulSoup(data)
+
+    print(soup)
   return "Goodbye World!\n"
 
 if __name__ == '__main__':
