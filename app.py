170325b0dac7c4af86f3df701682010b76b3ea05
Ryan Showalter <ryanshowalter76@gmail.com>
2020-02-12 19:29:01 -0500

    generate primes

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import random
 from flask import Flask
 app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
   return "Crappy website.\n"
+
+@app.route('/oop')
+def yuck():
+    largest_prime = random.randint(1, 500000000)
+
+    return f"Largest prime; {largest_prime}"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
