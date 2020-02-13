f766cee42121ab129d8a98683d33a5f738628acc
Zachary Tucker <tuckerzp@localhost.localdomain>
2020-02-12 19:17:23 -0500

    Add more Ripley

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "Goodbye World!\n"
+  return "Hello Ripley!\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
