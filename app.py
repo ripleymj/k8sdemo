40335d79aaad2fc121ff0b9a68707a7153ade878
Tony Pau <pautx@dukes.jmu.edu>
2020-02-12 19:11:11 -0500

    added img

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "<style>body {background-color: red;}</style><body><h1>Hello World!</h1>\n</body>"
+    return "<style>body {background-color: red;}</style><body><h1>Hello World!</h1>\n<img href='https://i.kym-cdn.com/photos/images/newsfeed/001/480/544/6c1.jpg' /></body>"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
