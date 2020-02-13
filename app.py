f9dfada112a14b1122787597fc79d14e47aa6678
Tony Pau <pautx@dukes.jmu.edu>
2020-02-12 19:07:17 -0500

    tried editing website

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "<style>body {background-color:
-  red;}</style><body><h1>Hello World!</h1>\n</body>"
+  return "<style>body {background-color: red;}</style><body><h1>Hello World!</h1>\n</body>"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
