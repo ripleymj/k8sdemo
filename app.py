4161bc9717adb4e297997cc07da1271a2d6d04e7
Cameron <bausercd@dukes.jmu.edu>
2020-02-12 19:05:20 -0500

    I made some dad changes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+
+
 from flask import Flask
 app = Flask('hello-cloudbuild')
 
 @app.route('/')
 def hello():
-  return "Hello World!\n"
+  import requests
+  head = {"Accept": "text/plain"}
+  req = requests.get('https://icanhazdadjoke.com/',headers=head)
+  return req.text
+  #return "Hello World!\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
